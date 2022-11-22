a20f97dadeeb3b9e06465894d6f1e175023cc4c7
Georgiy <stroganovgeorgi01@gmail.com>
2022-11-22 20:36:58 +0300

    Добавил 6 лабу

--- Lab_6/Lab_6.py
+++ Lab_6/Lab_6.py
@@ -1,24 +1,23 @@
-#import asyncio
-#import time
-
-#библиотека openweather
+# -*- coding: cp1251 -*-
 import requests
+from pprint import pprint
 from config import open_weather_token
 
+#Доделать нормальный вывод данных
 
-import telebot
-from telebot import types
-bot = telebot.TeleBot('5390890783:AAHgTuMn3jqD-yb8H5xc3uU0k7-C_krfQqU')
-
-
-def get_weather(city, open_weather_token):
+def get_weather(city):
     try:
+        data = requests.get("http://api.openweathermap.org/geo/1.0/direct?q={}&limit={}&appid={}".format(city, 5, open_weather_token)).json()
+        print("---------------------------------------------------------")
+        r_info = requests.get("https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=metric".format(data[0]["lat"], data[0]["lon"], open_weather_token)).json()
+        pprint(r_info)
+        print("---------------------------------------------------------")
+        city = r_info["name"]
+        weather_temp = r_info["main"]["temp"] # температура
+        weather_humidity = r_info["main"]["humidity"] # влажность
+        weather_pressure = r_info["main"]["pressure"] # давление
+        weather_wind = r_info["wind"]["speed"] # скорость ветра
+        #weather_...
+        print(f"Погода в городе: {city}\nТемпература: {weather_temp}\nВлажность: {weather_humidity}\nДавление: {weather_pressure}\nСкорость ветра: {weather_wind}")
     except Exception as ex:
-        print(ex)
-        print("Проверьте введеные данные")
-
-def main():
-    city =input("Введите город")
-    get_weather(city, open_weather_token)
-if __name__ == "__main__":
-    main()
+        print(ex, "Проверьте название города")
--- Lab_6/config.py
+++ Lab_6/config.py
@@ -1,1 +1,2 @@
-open_weather_token = ""
+open_weather_token = "a62487536f5cf45fa41bfd6cbab59ed0"
+tg_bot_token = "5390890783:AAHgTuMn3jqD-yb8H5xc3uU0k7-C_krfQqU"
